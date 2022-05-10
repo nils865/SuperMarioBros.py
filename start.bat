e83764c74556e57611403a8dbd8ee5c618b8275c
nils865 <nils.staas0806@gmail.com>
2022-05-10 12:50:22 +0200

    Changed Pause to exit

--- start.bat
+++ start.bat
@@ -9,4 +9,4 @@ curl -o maps/1-1.json https://raw.githubusercontent.com/nils865/SuperMarioBros.p
 curl -o maps/1-1-sub.json https://raw.githubusercontent.com/nils865/SuperMarioBros.py/main/maps/1-1-sub.json
 
 python SuperMarioBros.py
-exit
+pause
