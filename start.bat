8cadd749bba1f315c5ddc1fc03c2645718906302
nils865 <nils.staas0806@gmail.com>
2022-05-10 12:45:07 +0200

    Auto Download with start script

--- start.bat
+++ start.bat
@@ -1,2 +1,12 @@
+echo off
+
+mkdir SuperMarioBros
+cd SuperMarioBros
+mkdir maps
+
+curl -o SuperMarioBros.py https://raw.githubusercontent.com/nils865/SuperMarioBros.py/main/SuperMarioBros.py
+curl -o maps/1-1.json https://raw.githubusercontent.com/nils865/SuperMarioBros.py/main/maps/1-1.json
+curl -o maps/1-1-sub.json https://raw.githubusercontent.com/nils865/SuperMarioBros.py/main/maps/1-1-sub.json
+
 python SuperMarioBros.py
-pause
+exit
