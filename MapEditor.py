545d27413a7ae57912c984ad0f859b9a42c7272c
nils865 <nils.staas0806@gmail.com>
2022-05-12 10:49:36 +0200

    added draw

--- MapEditor.py
+++ MapEditor.py
@@ -11,9 +11,10 @@ except Exception:
     from keyboard import is_pressed
 
 height = 14
-name = input("Enter name: ")
-width = int(input("Enter width: ")) + 1
-bg = input("Enter Background color: ")
+name = "test" # input("Enter name: ")
+width = 20 # int(input("Enter width: ")) + 1
+bg = "w" # input("Enter Background color: ")
+scrolls = True # bool(input("Enter if level scrolls: "))
 
 data = {}
 
@@ -27,6 +28,59 @@ data["bgColor"] = bg
 for i in range(height):
     for j in range(width):
         data["maps"][i].append(bg)
+
+game = data["maps"]
+scroll = 0
+with open("settings.json", "r") as f:
+    data = json.load(f)
+    framerate = 1 / data["framerate"]
+    color = data["color"]
+
+def setcolor(r,g,b):
+    return(f"\x1b[38;2;{r};{g};{b}m")
+
+if os.name == 'nt':
+    def clear():
+        os.system('cls')
+else:
+    def clear():
+        os.system('clear')
+
+def draw():
+    global game, color, scroll, scrolls
+    out = ""
+
+    for i in range(height - 1):
+        j = 0
+        if scrolls:
+            j = scroll
+        while j < ((width - 1) + scroll):
+            out += setcolor(color[game[i][j]][0], color[game[i][j]][1], color[game[i][j]][2])
+            out += ("██")
+            try:
+                while game[i][j] == game[i][j + 1] and j < width:
+                    out += ("██")  
+                    j += 1
+            except Exception:
+                pass
+            j += 1
+        out += ("\n")
+    out += ("\x1b[0m")  
+
+    return out
+
+def goUp(lines):
+   sys.stdout.write("\x1b[1A" * lines) 
+
+clear()
+while True:
+    t = perf_counter()
+    goUp((height))
+    
+    print(draw())
+
+    if (perf_counter() - t) < framerate:
+        sleep(framerate - (perf_counter() - t))
 
 with open(f"maps/{name}.json", "w") as f:
     json.dump(data, f)
